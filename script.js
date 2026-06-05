0103682eaf3f4a40672334acd631bf87b04406c2
Mithresh M <mithreshmandhrasalam59@gmail.com>
2026-06-05 21:36:17 +0530

    Define user records for login

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var currentStudentId = "";
 var currentStudentName = "";
 var currentQuestionIndex = 0;
 var score = 0;
+var users = {
+  "student1": "Arjun Sharma",
+  "student2": "Priya Mehta",
+  "student3": "Rahul Verma",
+  "student4": "Sneha Patel"
+};
