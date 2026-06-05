bcf47a7ff35cb1215ac0de3986338be34e444f2a
Mithresh M <mithreshmandhrasalam59@gmail.com>
2026-06-05 21:36:17 +0530

    Add array shuffle utility function

--- script.js
+++ script.js
@@ -55,3 +55,13 @@ function login() {
     document.getElementById("quiz-section").style.display = "block";
   }
 }
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
